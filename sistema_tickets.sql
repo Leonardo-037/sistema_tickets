d4c9c2a1253e6d034be6feabed3e38f98895a940
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2025-07-28 15:32:25 -0400

    up

--- sistema_tickets.sql
+++ sistema_tickets.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-07-2025 a las 21:03:21
+-- Tiempo de generación: 28-07-2025 a las 21:32:03
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 7.4.29
 
